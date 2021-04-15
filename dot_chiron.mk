9e369f3f04a0be8e6dacbacf56e96efeb579f156
Marcin Winter <zalunai32@gmail.com>
2021-04-15 09:02:43 +0200

    Cleanup dot_chiron.mk

--- dot_chiron.mk
+++ dot_chiron.mk
@@ -14,30 +14,25 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
+# Inherits
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some chiron stuff
 $(call inherit-product, device/xiaomi/chiron/device.mk)
-$(call inherit-product-if-exists, vendor/apps/GoogleCamera/config.mk)
-
-# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/dot/config/common.mk)
 
+# Device settings
 PRODUCT_NAME := dot_chiron
 PRODUCT_DEVICE := chiron
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 2
 PRODUCT_MANUFACTURER := Xiaomi
 DEVICE_MAINTAINER := WhiterWalt
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
+BUILD_FINGERPRINT := Xiaomi/chiron/chiron:8.0.0/OPR1.170623.027/V9.5.4.0.ODEMIFA:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="chiron-user 8.0.0 OPR1.170623.027 V9.5.4.0.ODEMIFA release-keys"
 
-BUILD_FINGERPRINT := Xiaomi/chiron/chiron:8.0.0/OPR1.170623.027/V9.5.4.0.ODEMIFA:user/release-keys
-
-WITH_GAPPS := true
+# dotOS Settings
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_BLUR := true
+WITH_GAPPS := true
