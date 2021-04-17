2e926e6f2e22a49d429ce1d843847b34db7cbd18
WhiterWalt <zalunai32@gmail.com>
2021-04-17 16:21:04 +0000

    Change DPI to 403 and add vendor.qti.hardware.vibrator.service

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 # AAPT
-PRODUCT_AAPT_PREF_CONFIG := 400dpi
+PRODUCT_AAPT_PREF_CONFIG := 403dpi
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
@@ -35,6 +35,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/synaptics_dsx.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/synaptics_dsx.kl \
     $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
+
+PRODUCT_COPY_FILES += \
+      vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # Properties
 include $(LOCAL_PATH)/vendor_prop.mk
